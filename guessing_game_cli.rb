5774d0e2f5d32d6ebcd3c30845d192996231be2a
Robert M Ricci <rmricci121@gmail.com>
2019-12-07 11:01:01 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,6 +1,6 @@
 # Code your solution here!
 def run_guessing_game
-  puts "Guess a number 1 through 6"
+  puts "/Guess a number 1 through 6"
 random_number = 1 + rand(6) 
   input = gets.chomp
 if input == random_number 
