48dfb5af33b3fd85389b8d398300fd356008709f
Robert M Ricci <rmricci121@gmail.com>
2019-12-07 11:07:22 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,7 +2,7 @@
 def run_guessing_game
   #puts "/Guess a number 1 through 6"
   random_number = 1 + rand(6) 
-  input = gets.chomp
+  input = gets.chomp.to_s
     if input == random_number 
       puts "You guessed the correct number"
     elsif  input != random_number
